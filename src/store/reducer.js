bc2548322198bc1a62a4c58326461e8d8920aca6
Juha Vartiamäki <beizdeveloper@gmail.com>
2018-09-26 16:00:38 +0300

    store

---
+++ src/store/reducer.js
@@ -0,0 +1,76 @@
+import * as actionTypes from './actions'
+
+
+const initialState = {
+
+  ingredients: {
+    salad: 0,
+    bacon: 0,
+    cheese: 0,
+    meat: 0
+
+  },
+  
+  totalPrice: 4
+};
+
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  cheese: 1,
+  meat: 1, 
+  bacon: 0.5
+}
+
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    // tässä levitetään staten ingredientsit 
+    // uuteen ingredients olioon sisään,
+    // pelkkä ingredients osoittaisi suoraan initialStaten olioon
+
+    // HUOMIO [] ei ole tässä taulukko
+    // otetaan Ingredients 
+    case actionTypes.ADD_INGREDIENT:
+      for (let element in state.ingredients){
+        console.log( "ADD_INGREDIENT: "+ element + " : " + state.ingredients[element] );
+      }
+      return {
+        ...state,
+        ingredients: {
+          ...state.ingredients,
+          [action.ingredientName]: state.ingredients[action.ingredientName] + 1
+        },
+        totalPrice:  state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
+      } 
+
+
+    case actionTypes.REMOVE_INGREDIENT:
+      console.log(" before " + action.ingredientName + " : " + state.ingredients[action.ingredientName])
+      
+      const igcopy ={
+        ...state.ingredients
+      }
+      for (let element in igcopy){
+
+        console.log( "REMOVE_INGR " + element + " - " + igcopy[element] ); 
+      }
+
+      return {
+        ...state,
+        ingredients: {
+          [action.ingredientName]: state.ingredients[action.ingredientName] - 1
+        },
+        totalPrice:  state.totalPrice - INGREDIENT_PRICES[action.ingredientName]
+       
+      }
+     
+
+    // jos ei oo mitään niin pitää defaulttistate palauttaa
+    default:
+      return state;
+
+
+  }
+};
+
+export default reducer;
